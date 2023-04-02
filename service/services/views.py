8bc9e97ff0bc08b7c73cc9eb243413d13af2107e
vasiliinovikov <novonovo19777@gmail.com>
2023-04-02 18:01:30 +0400

    day 3 - 1

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -10,6 +10,7 @@ from services.serializers import SubscriptionSerializer
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    )
+    ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
