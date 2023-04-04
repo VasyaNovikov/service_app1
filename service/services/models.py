29953fd26ea74fabe9a8d632ecce920bf740ae33
vasiliinovikov <novonovo19777@gmail.com>
2023-04-04 07:56:58 +0400

    day 6 - 1

--- service/services/models.py
+++ service/services/models.py
@@ -57,6 +57,7 @@ class Subscription(models.Model):
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default='')
 
     def __str__(self):
         return f"Subscription: {self.plan}"
